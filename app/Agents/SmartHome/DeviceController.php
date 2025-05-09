d9b5352f437e6122832d66042270f0d2e38fbe20
Giorgos Kalogeitonas <gkalog2@gmail.com>
2025-05-09 16:41:32 +0300

    feat: Enhance DeviceController with logging functionality

--- app/Agents/SmartHome/DeviceController.php
+++ app/Agents/SmartHome/DeviceController.php
@@ -2,25 +2,47 @@
 
 namespace App\Agents\SmartHome;
 
+use App\Support\Logging\LoggerFactory;
+
+
+
 class DeviceController
 {
+
+    protected $logger;
+    public function __construct()
+    {
+        $this->logger = LoggerFactory::create('DeviceController');
+    }
+
+
     public function turnOnLight(): string
     {
+        $this->log("Turning on the light.");
         return "The light has been turned on!!!";
     }
 
     public function turnOffLight(): string
     {
+        $this->log("Turning off the light.");
         return "The light has been turned off.";
     }
 
     public function turnOnAirConditioner(): string
     {
+        $this->log("Turning on the air conditioner.");
         return "The air conditioner has been turned on.";
     }
 
     public function turnOffAirConditioner(): string
     {
+        $this->log("Turning off the air conditioner.");
         return "The air conditioner has been turned off.";
+    }
+
+
+    private function log(string $message): void
+    {
+        $this->logger->info($message);
     }
 }
